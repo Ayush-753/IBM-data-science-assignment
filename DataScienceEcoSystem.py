8f368d3677714736df2e859811b4da33bc4d51f9
Ayush Kamath <126310799+Ayush-753@users.noreply.github.com>
2024-06-09 20:48:13 +0530

    Add files via upload

---
+++ DataScienceEcoSystem.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+# 
+
+# Some of the popular languages that Data Scientists use are:
+
+# 1. Python
+# 2. R
+# 3. SQL
+# 4. Julia
+# 5. Scala
+
+# Some of the commonly used libraries used by Data Scientists include:
+
+# 1. NumPy
+# 2. Pandas
+# 3. Scikit-Learn
+# 4. Matplotlib
+# 4. Seaborn
+
+# Data Science Tools
+
+# | Data Science Tools  |
+# |---------------------|
+# | Jupyter Notebook    |
+# | RStudio             |
+# | Apache Zeppelin     |
+# 
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+# 
+# 1. Python
+# 2. R
+# 3. SQL
+
+# # This is a simple arithmetic expression to multiply then add integers
+# result = (3 * 4) + 5
+# 
+# 17
+
+# **Objectives:**
+# 
+# - List popular languages for Data Science
+# - Understand the difference between supervised and unsupervised learning
+# - Learn when to use Multiple Linear Regression
+# - Identify the requirements for independent and dependent variables in regression
+# - Create and evaluate simple arithmetic expressions in Python
+# 
+
+# ## Author
+# 
+# Ayush U Kamath
+# 
+
+# In[ ]:
+
+
+
+
